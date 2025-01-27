4de59eb96f7f5e616be4e05be64419a9ff3d0202
Aurelien <aurelien.fillion.pro@gmail.com>
2025-01-27 13:38:32 +0100

    build: config de la map

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Vector2 _movement;
+    private Rigidbody2D _rigidbody2D;
+    public float speed = 5f;
+
+    void Awake()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        Debug.Log("Start Player");
+    }
+
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        _movement = new Vector2(horizontal, vertical);
+    }
+
+    void FixedUpdate()
+    {
+        _rigidbody2D.linearVelocity = _movement * speed;
     }
 }
